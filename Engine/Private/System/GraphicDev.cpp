8c0bab89b4ebc39880c3ee8a53c96a27720aeb5d
Lee Hak Jun <harleybird23@gmail.com>
2023-11-02 16:46:39 +0900

    디바이스 초기화 수정

--- Engine/Private/System/GraphicDev.cpp
+++ Engine/Private/System/GraphicDev.cpp
@@ -4,10 +4,89 @@ CGraphicDev::CGraphicDev()
 {
 }
 
-HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVsync, HWND hWnd, _bool bFullScreen,
-    _float fScreenDepth, _float fScreenNear)
+HRESULT CGraphicDev::Initialize(const FDEVICE_INIT& tInit)
 {
-    m_bVsync_Enabled = bVsync;
+    FAILED_CHECK_RETURN(Ready_SwapChain(tInit), E_FAIL);
+
+    FAILED_CHECK_RETURN(Ready_Depth(tInit), E_FAIL);
+
+    FAILED_CHECK_RETURN(Ready_Stencil(tInit), E_FAIL);
+
+    FAILED_CHECK_RETURN(Ready_Viewport(tInit), E_FAIL);
+
+	return S_OK;
+}
+
+HRESULT CGraphicDev::Clear_BackBuffer_View(_float4 vClearColor)
+{
+    _float color[4] = { vClearColor.x, vClearColor.y, vClearColor.z, vClearColor.w };
+
+    // 백버퍼 지우기
+    m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
+
+    return S_OK;
+}
+
+HRESULT CGraphicDev::Clear_DepthStencil_View()
+{
+    // 깊이 버퍼 지우기
+    m_pDeviceContext->ClearDepthStencilView(m_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.f, 0);
+
+    return S_OK;
+}
+
+HRESULT CGraphicDev::Present()
+{
+    // 렌더링 완료되어 화면에 백 버퍼를 표시
+    if (m_bVsync_Enabled)
+    {
+        // 화면 새로 고침 비율 고정
+        m_pSwapChain->Present(1, 0);
+    }
+    else
+    {
+        // 가능한 빠르게 출력
+        m_pSwapChain->Present(0, 0);
+    }
+
+    return S_OK;
+}
+
+CGraphicDev* CGraphicDev::Create(const FDEVICE_INIT& tInit)
+{
+    ThisClass* pInstance = new ThisClass();
+
+    if (FAILED(pInstance->Initialize(tInit)))
+    {
+        Engine::Safe_Release(pInstance);
+
+        MSG_BOX("CGraphicDev Create Failed");
+
+        return nullptr;
+    }
+
+    return pInstance;
+}
+
+void CGraphicDev::Free()
+{
+    if (m_pSwapChain)
+    {
+        m_pSwapChain->SetFullscreenState(false, NULL);
+    }
+    Safe_Release(m_pRasterState);
+    Safe_Release(m_pDepthStencilView);
+    Safe_Release(m_pDepthStencilState);
+    Safe_Release(m_pDethStencilBuffer);
+    Safe_Release(m_pRenderTargetView);
+    Safe_Release(m_pDeviceContext);
+    Safe_Release(m_pDevice);
+    Safe_Release(m_pSwapChain);
+}
+
+HRESULT CGraphicDev::Ready_SwapChain(const FDEVICE_INIT& tInit)
+{
+    m_bVsync_Enabled = tInit.bVSync;
 
     // 그래픽 인터페이스 팩토리 생성
     IDXGIFactory* factory = nullptr;
@@ -24,7 +103,7 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
     // 출력 (모니터)에 대한 DXGI_FORMAT_R8G8B8A8_UNORM 표시 형식에 맞는 모드 수를 가져옴
     _uint iNumModes = 0;
     FAILED_CHECK_RETURN(adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED,
-                        &iNumModes, NULL), E_FAIL);
+        &iNumModes, NULL), E_FAIL);
 
     // 가능한 모든 모니터와 그래픽카드 조합을 저장할 리스트 생성
     DXGI_MODE_DESC* displayModeList = new DXGI_MODE_DESC[iNumModes];
@@ -32,8 +111,8 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
         return E_FAIL;
 
     // 디스플레이 모드에 대한 리스트를 채운다.
-    FAILED_CHECK_RETURN(adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, 
-                        &iNumModes, displayModeList), E_FAIL);
+    FAILED_CHECK_RETURN(adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED,
+        &iNumModes, displayModeList), E_FAIL);
 
 
     // 이모든 디스플레이 모드에 대해 화면 너비/높이에 맞는 디스플레이모드 찾기
@@ -80,9 +159,11 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
     // 팩토리 객체 해제
     Safe_Release(factory);
 
+    //-----------------------------------------------
+
     DXGI_SWAP_CHAIN_DESC swapChainDesc;
     ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
-	
+
     swapChainDesc.BufferCount = 1;          // 백버퍼 개수
     swapChainDesc.BufferDesc.Width = 0;     // 백버퍼 너비 높이
     swapChainDesc.BufferDesc.Height = 0;
@@ -102,14 +183,14 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 
     // 렌더링에 사용될 윈도우 핸들 지정
-    swapChainDesc.OutputWindow = hWnd;
+    swapChainDesc.OutputWindow = tInit.hWnd;
 
     // 멀티샘플링을 끈다
     swapChainDesc.SampleDesc.Count = 1;
     swapChainDesc.SampleDesc.Quality = 0;
 
     // 전체화면
-    swapChainDesc.Windowed = !bFullScreen;
+    swapChainDesc.Windowed = !tInit.bFullScreen;
 
     // 출력 후 백버퍼를 비우도록 지정
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
@@ -133,14 +214,18 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
 
     Safe_Release(backBufferPtr);
 
+    return S_OK;
+}
 
+HRESULT CGraphicDev::Ready_Depth(const FDEVICE_INIT& tInit)
+{
     // 깊이 버퍼 구조체를 초기화한다.
     D3D11_TEXTURE2D_DESC depthBufferDesc;
     ZeroMemory(&depthBufferDesc, sizeof(depthBufferDesc));
 
     // 깊이 버퍼 구조체를 작성한다.
-    depthBufferDesc.Width = iScreenWidth;
-    depthBufferDesc.Height = iScreenHeight;
+    depthBufferDesc.Width = tInit.iScreenWidth;
+    depthBufferDesc.Height = tInit.iScreenHeight;
     depthBufferDesc.MipLevels = 1;
     depthBufferDesc.ArraySize = 1;
     depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
@@ -153,8 +238,12 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
 
     // 설정된 깊이버퍼 구조체를 사용하여 깊이 버퍼 텍스쳐를 생성한다.
     FAILED_CHECK_RETURN(m_pDevice->CreateTexture2D(&depthBufferDesc, NULL, &m_pDethStencilBuffer), E_FAIL);
-    
 
+    return S_OK;
+}
+
+HRESULT CGraphicDev::Ready_Stencil(const FDEVICE_INIT& tInit)
+{
     // 스텐실 상태 구조체를 초기화한다.
     D3D11_DEPTH_STENCIL_DESC depthStencilDesc;
     ZeroMemory(&depthStencilDesc, sizeof(depthStencilDesc));
@@ -214,100 +303,36 @@ HRESULT CGraphicDev::Initialize(_int iScreenWidth, _int iScreenHeight, _bool bVs
     // 방금 작성한 구조체에서 래스터 라이저 상태를 만든다.
     FAILED_CHECK_RETURN(m_pDevice->CreateRasterizerState(&rasterDesc, &m_pRasterState), E_FAIL);
 
-
     m_pDeviceContext->RSSetState(m_pRasterState);
 
+    return S_OK;
+}
+
+HRESULT CGraphicDev::Ready_Viewport(const FDEVICE_INIT& tInit)
+{
     D3D11_VIEWPORT viewport;
-    viewport.Width = (_float)iScreenWidth;
-    viewport.Height = (_float)iScreenHeight;
-    viewport.MinDepth = 0.0f;
-    viewport.MaxDepth = 1.f;
     viewport.TopLeftX = 0.f;
     viewport.TopLeftY = 0.f;
+    viewport.Width = (_float)tInit.iScreenWidth;
+    viewport.Height = (_float)tInit.iScreenHeight;
+    viewport.MinDepth = 0.0f;
+    viewport.MaxDepth = 1.f;
 
     // 뷰포트를 생성
     m_pDeviceContext->RSSetViewports(1, &viewport);
 
     // 투영 행렬을 설정
     _float fFieldOfView = 3.141592654f / 4.f;
-    _float fScreenAspect = (_float)iScreenWidth / (_float)iScreenHeight;
+    _float fScreenAspect = (_float)tInit.iScreenWidth / (_float)tInit.iScreenHeight;
 
     // 3D 렌더링을 위한 투영 행렬을 만든다.
-    m_matProjection = XMMatrixPerspectiveFovLH(fFieldOfView, fScreenAspect, fScreenNear, fScreenDepth);
+    m_matProjection = XMMatrixPerspectiveFovLH(fFieldOfView, fScreenAspect, tInit.fScreenNear, tInit.fScreenDepth);
 
     // 세계 행렬을 항등 행렬로 초기화
     m_matWorld = XMMatrixIdentity();
 
     // 2D 렌더링을 위한 직교 투영 행렬을 만든다.
-    m_matOrtho = XMMatrixOrthographicLH((_float)iScreenWidth, (_float)iScreenHeight, fScreenNear, fScreenDepth);
-
-	return S_OK;
-}
-
-HRESULT CGraphicDev::Clear_BackBuffer_View(_float4 vClearColor)
-{
-    _float color[4] = { vClearColor.x, vClearColor.y, vClearColor.z, vClearColor.w };
-
-    // 백버퍼 지우기
-    m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
+    m_matOrtho = XMMatrixOrthographicLH((_float)tInit.iScreenWidth, (_float)tInit.iScreenHeight, tInit.fScreenNear, tInit.fScreenDepth);
 
     return S_OK;
-}
-
-HRESULT CGraphicDev::Clear_DepthStencil_View()
-{
-    // 깊이 버퍼 지우기
-    m_pDeviceContext->ClearDepthStencilView(m_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.f, 0);
-
-    return S_OK;
-}
-
-HRESULT CGraphicDev::Present()
-{
-    // 렌더링 완료되어 화면에 백 버퍼를 표시
-    if (m_bVsync_Enabled)
-    {
-        // 화면 새로 고침 비율 고정
-        m_pSwapChain->Present(1, 0);
-    }
-    else
-    {
-        // 가능한 빠르게 출력
-        m_pSwapChain->Present(0, 0);
-    }
-
-    return S_OK;
-}
-
-CGraphicDev* CGraphicDev::Create(_int iScreenWidth, _int iScreenHeight, 
-    _bool bVsync, HWND hWnd, _bool bFullScreen, _float fScreenDepth, _float fScreenNear)
-{
-    ThisClass* pInstance = new ThisClass();
-
-    if (FAILED(pInstance->Initialize(iScreenWidth, iScreenHeight, bVsync, hWnd, bFullScreen, fScreenDepth, fScreenNear)))
-    {
-        Engine::Safe_Release(pInstance);
-
-        MSG_BOX("CGraphicDev Create Failed");
-
-        return nullptr;
-    }
-
-    return pInstance;
-}
-
-void CGraphicDev::Free()
-{
-    if (m_pSwapChain)
-    {
-        m_pSwapChain->SetFullscreenState(false, NULL);
-    }
-    Safe_Release(m_pRasterState);
-    Safe_Release(m_pDepthStencilView);
-    Safe_Release(m_pDepthStencilState);
-    Safe_Release(m_pDethStencilBuffer);
-    Safe_Release(m_pRenderTargetView);
-    Safe_Release(m_pDeviceContext);
-    Safe_Release(m_pDevice);
-    Safe_Release(m_pSwapChain);
 }
