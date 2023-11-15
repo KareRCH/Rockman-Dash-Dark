25447024f5494916571153a552c18633f149082c
Lee Hak Jun <harleybird23@gmail.com>
2023-11-15 20:14:58 +0900

    정점에 대한 뼈 Weight 정보 저장 기능 추가 | 머터리얼 분석중

--- Reference/Header/Engine/System/ModelMgr.h
+++ Reference/Header/Engine/System/ModelMgr.h
@@ -13,7 +13,7 @@ class FMeshData final
 private:
 	explicit FMeshData() {}
 	explicit FMeshData(const FMeshData& rhs)
-		: vecVertices(rhs.vecVertices), vecIndices(rhs.vecIndices), vecBoneName(rhs.vecBoneName)
+		: vecVertices(rhs.vecVertices), vecIndices(rhs.vecIndices), vecBone(rhs.vecBone)
 		, iMaterIndex(rhs.iMaterIndex), matTransform(rhs.matTransform)
 	{
 	}
@@ -57,16 +57,24 @@ public:
 	{
 		vecVertices.clear();
 		vecIndices.clear();
-		vecBoneName.clear();
+		vecBone.clear();
 		delete this;
 	}
 
 public:
-	vector<VERTEX_MODEL>	vecVertices;
-	vector<_uint>			vecIndices;
-	vector<wstring>			vecBoneName;
-	_uint					iMaterIndex;
-	_float4x4				matTransform;
+	struct FVertexBoneData
+	{
+		wstring			strName;
+		vector<_uint>	vecVtxID;
+		vector<_float>	vecWeights;
+	};
+
+public:
+	vector<VERTEX_MODEL>		vecVertices;
+	vector<_uint>				vecIndices;
+	vector<FVertexBoneData>		vecBone;
+	_uint						iMaterIndex;
+	_float4x4					matTransform;
 };
 
 /// <summary>
@@ -215,6 +223,15 @@ public:
 			return nullptr;
 
 		return (*iter).second;
+	}
+
+	void Add_Bone(const wstring& strBoneKey, FBoneData* const pBoneData)
+	{
+		auto iter = mapBoneData.find(strBoneKey);
+		if (iter != mapBoneData.end())
+			return;
+
+
 	}
 
 public:
@@ -368,7 +385,7 @@ public:
 	}
 
 public:
-	const FAnimData* const	Get_Anim(const wstring& strAnimKey)
+	const FAnimData* const	Get_AnimData(const wstring& strAnimKey)
 	{
 		auto iter = mapAnimData.find(strAnimKey);
 		if (iter == mapAnimData.end())
@@ -467,7 +484,12 @@ private:
 	string	m_strMainDir = "";			// 참조할 메인 디렉터리
 
 public:
-	void					Load_Model(const string& strFileName, const wstring& strGroupKey);
+	void	Load_Model(const string& strFileName, const wstring& strGroupKey);
+	void	Load_MeshBoneMaterial(FModelGroup* pModelGroup);
+	void	Load_Anim(FAnimGroup* pAnimGroup);
+	void	Load_Hierarchi(FModelGroup* pModelGroup);
+
+
 	const FMeshData* const	Get_Mesh(const wstring& strGroupKey, const wstring& strMeshKey);
 
 private:
